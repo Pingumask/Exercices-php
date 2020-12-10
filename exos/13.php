4597f11035215d3b0a57dbd0207f4354f4ac2ef8
Pingumask <Pingumask@elanta.fr>
2020-12-10 18:48:53 +0100

    Ajout de 2 exercices oubliés précédement

--- exos/13.php
+++ exos/13.php
@@ -1,1 +1,10 @@
 <p>Grâce à la fonction rand(), remplir un tableau et vérifier si le chiffre 13 est présent et afficher un message en conséquence.</p>
+<?php
+for ($i = 0; $i < 20; $i++) {
+  $array[] = rand(0, 20);
+}
+var_dump($array);
+echo "Ce tableau ";
+echo in_array(13, $array) ? "contient" : "ne contient pas";
+echo " le nombre 13";
+?>
--- exos/capitales.php
+++ exos/capitales.php
@@ -2,3 +2,22 @@
   Creer un tableau qui stock les pays d'europe avec leur capitale.
   Afficher ce tableau via une boucle
 </p>
+<?php
+$capitales = [
+  "France" => "Paris",
+  "Espagne" => "Madrid",
+  "Belgique" => "Bruxelles"
+];
+echo "<table>
+        <tr>
+          <th>Pays</th>
+          <th>Capitale</th>
+        </tr>";
+foreach ($capitales as $pays => $capitale) {
+  echo "<tr>
+          <th>$pays</th>
+          <td>$capitale</td>
+        </tr>";
+}
+echo "</table>";
+?>
